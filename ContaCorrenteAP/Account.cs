b7ba523c4d2545abb5cd28fcf8d3b3fb50fad678
danilo <danilocencigg@gmail.com>
2025-04-02 17:42:48 -0300

    Add project files.

---
+++ ContaCorrenteAP/Account.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContaCorrenteAP
+{
+    public class Account
+    {
+        public string name;
+        public string password;
+        public string identifier;
+        public double balance;
+        public double limit;
+        public List<string> AccountHistory;
+
+        public void Withdrawal(double value)
+        {
+            balance -= value;
+            AccountHistory.Add($"Saque: -R${value}");
+        }
+
+        public void Deposit(double value)
+        {
+            balance += value;
+            AccountHistory.Add($"Deposito: +R${value}");
+        }
+
+        public void TransferTo(Account userAccount2, double value)
+        {
+            Withdrawal(value);
+            userAccount2.Deposit(value);
+            
+        }
+
+        public void ShowAccountHistory()
+        {
+            for (int i = 0; i < AccountHistory.Count; i++)
+            {
+                Console.WriteLine(AccountHistory[i]);
+            }
+            Console.WriteLine("Pressione Enter para Sair.\n");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ContaCorrenteAP/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ContaCorrenteAP
+{
+    internal class Program
+    {
+        public static void CreateAccount()
+        {
+            Account userAccount1 = new Account();
+            userAccount1.name = "Robson";
+            userAccount1.password = "1234";
+            userAccount1.identifier = "RGK4667JF";
+            userAccount1.balance = 1000;
+            userAccount1.AccountHistory = new List<string> { };
+
+            userAccount1.Withdrawal(200);
+            userAccount1.Deposit(200);
+            userAccount1.Deposit(200);
+            userAccount1.Withdrawal(200);
+
+            Account userAccount2 = new Account();
+            userAccount2.name = "Ricardo";
+            userAccount2.password = "56789";
+            userAccount2.identifier = "MGT3378IP";
+            userAccount2.balance = 1000;
+            userAccount2.AccountHistory = new List<string> { };
+
+            userAccount1.TransferTo(userAccount2, 400);
+            userAccount1.ShowAccountHistory();
+        }
+
+
+        static void Main(string[] args)
+        {
+            CreateAccount();
+        }
+    }
+}
